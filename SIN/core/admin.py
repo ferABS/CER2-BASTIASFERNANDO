b4bceda342dd6322d16418d8eb971a2c8adc110a
FerABS <fernandobastias103@gmail.com>
2023-10-22 17:22:36 -0300

    Agregado y configurado el superusuario y los modelos, falta construir la pagina inicio con su responsividad y las restricciones de la creacion de subusuarios con sus correspondientes grupos

--- SIN/core/admin.py
+++ SIN/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Comunicado, Entidad
 
 # Register your models here.
+admin.site.register(Comunicado)
+admin.site.register(Entidad)
--- SIN/core/models.py
+++ SIN/core/models.py
@@ -1,17 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+#Departamentos
 class Entidad(models.Model):
     id = models.BigAutoField(primary_key = True)
-    nombre = models.CharField(max_length=30)
-    logo = models.ImageField()
+    nombre = models.CharField(max_length=30, help_text="Ingrese el nombre del departamento")
+    logo = models.ImageField(help_text="Añada el logo del departamento")
 
+    def __str__(self):
+        return self.nombre
+
+TIPO_CHOICES = (
+    ("S", "Suspensión de actividades"),
+    ("C", "Suspensión de clases"),
+    ("I", "Información")
+)
+
+#Comunicados
 class Comunicado(models.Model):
     id = models.BigAutoField(primary_key = True)
-    titulo = models.CharField(max_length=50)
-    detalle = models.CharField(max_length=500)
+    titulo = models.CharField(max_length=50, help_text="Ingrese el titulo del comunicado")
+    detalle = models.TextField()
     detalle_corto = models.CharField(max_length=300)
-    entidad = models.ForeignKey(Entidad, on_delete=models.CASCADE)
+    entidad = models.ForeignKey(Entidad, on_delete=models.CASCADE, help_text="Eliga el departamento asociado")
     visible = models.BooleanField()
-    fecha_publicacion = models.DateTimeField()
-    fecha_ultima_publicacion = models.DateTimeField()
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    fecha_ultima_modificacion = models.DateTimeField(auto_now=True)
+    tipo = TIPO_CHOICES
+
+    def __str__(self):
+        return self.titulo
