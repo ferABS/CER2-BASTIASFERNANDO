d719c5acb999b051131f1fbfa610277626fdcaac
FerABS <fernandobastias103@gmail.com>
2023-10-23 15:41:09 -0300

    hecha la pagina principal, añadir presentacion de tarjetas y admins

--- SIN/core/views.py
+++ SIN/core/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Comunicado, Entidad
 
 # Create your views here.
 def index(request):
     title = "Inicio"
-
+    entidad = Entidad.id
+    numComunicados = Comunicado.objects.filter(visible = True).count()
+    
     data = {
-        "title": title
+        "title": title,
+        "numComunicados": numComunicados,
+        "entidad": entidad,
     }
 
     return render(request, 'core/index.html', data)
